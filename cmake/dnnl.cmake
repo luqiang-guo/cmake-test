7fb85e641a09a42d542aa1ee31243159e8106cd5
luqiang-guo <702572275@qq.com>
2022-01-04 18:59:19 +0800

    dnnl.cmake

--- cmake/dnnl.cmake
+++ cmake/dnnl.cmake
@@ -1,4 +1,3 @@
-# find_package(Threads REQUIRED)
 include(FetchContent) 
 set(FETCHCONTENT_QUIET off)
 
@@ -13,23 +12,7 @@ set(DNNL_CPU_RUNTIME "TBB" CACHE STRING "")
 set(DNNL_BUILD_EXAMPLES OFF CACHE BOOL "")
 set(DNNL_BUILD_TESTS OFF CACHE BOOL "")
 # set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
-FetchContent_GetProperties(dnnl)
 # set(CMAKE_POLICY_DEFAULT_CMP0079 NEW)
-
-# if(NOT dnnl)
-#   FetchContent_Populate(tbbbb)
-#   add_subdirectory(${tbbbb_SOURCE_DIR} ${tbbbb_BINARY_DIR})
-# endif()
+FetchContent_GetProperties(dnnl)
 
 fetchcontent_makeavailable(dnnl)
-
-add_dependencies(dnnl tbb)
-
-install(TARGETS test_tblgen LLVMTableGen LLVMDemangle LLVMSupport COMPONENT OneFlowTableGen)
-add_custom_target(install-oneflow-tblgen
-  DEPENDS test_tblgen
-  COMMAND
-      "${CMAKE_COMMAND}" -DCMAKE_INSTALL_COMPONENT=OneFlowTableGen
-      -P "${CMAKE_BINARY_DIR}/cmake_install.cmake"
-)
-
